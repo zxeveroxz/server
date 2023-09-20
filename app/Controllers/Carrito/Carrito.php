fa44d4efcd99b98dfec9288a8ee72d9dce5f9d05
Sergio Zegarra <s.zegarra@hotmail.com>
2023-09-20 01:43:59 -0500

    mas avanzes de las entidades

--- app/Controllers/Carrito/Carrito.php
+++ app/Controllers/Carrito/Carrito.php
@@ -1,20 +1,38 @@
-<?php namespace App\Controllers\Carrito;
+<?php
+
+namespace App\Controllers\Carrito;
 
 use App\Controllers\BaseController;
+use CodeIgniter\Model;
 
+class Carrito extends BaseController
+{
 
-class Carrito extends BaseController{
-    public function index(){
+    public $RUC;
+    public $RAZON;
+
+    public function __construct()
+    {
         $uri = service('uri');
-        print_r($uri);
-        return "pagina de carrito de compra para el publico";
+        $this->RUC = $uri->getSegments()[0];
+        $empresa = model("EmpresaModel");
+        $rows =  $empresa->where('empresa_ruc',$this->RUC)->first(); 
+
+        $this->RAZON = $rows->empresaInfo->empresa_razon;
+        
     }
 
-    public function demo(){
-        $uri = service('uri');
-        print_r($uri);
-     
-        return "pagina de carrito de compra para el publico";
+    public function index()
+    {
+
+        return "<h1>$this->RAZON</h1>pagina de carrito de compra para el publico";
     }
 
+    public function demo()
+    {
+        $uri = service('uri');
+        print_r($uri);
+
+        return "pagina de carrito de compra para el publico";
+    }
 }
